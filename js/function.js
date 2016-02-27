2ba6c7cb078dfd59786685878568f03bb43bf16f
rodolfohans <rodolfohans@gmail.com>
2016-02-27 17:36:03 -0300

    teste de burger

---
+++ js/function.js
@@ -0,0 +1,30 @@
+$(document).ready(function inicio() {
+	smoth_scroll();
+	header_position();
+});
+
+function smoth_scroll() {
+	$('a[href^="#"]').on('click',function (e) {
+		e.preventDefault();
+
+		var target = this.hash,
+		$target = $(target);
+
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 700, 'swing', function() {
+			window.location.hash = target;
+		});
+	});
+}
+
+function header_position() {
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1){
+			document.getElementById("p2").style.position="fixed";
+		}
+		else {
+			document.getElementById("p2").style.position="absolute";
+		}
+	});
+}
